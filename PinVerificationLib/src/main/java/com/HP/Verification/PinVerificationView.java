e9a837cb057d34f277970c0ffbb973f10cb4bff2
oyin25 <ajibadedamola25@gmail.com>
2025-04-27 09:37:33 +0100

    PinView

---
+++ PinVerificationLib/src/main/java/com/HP/Verification/PinVerificationView.java
@@ -0,0 +1,371 @@
+package com.HP.Verification;
+
+import android.animation.*;
+import android.animation.ObjectAnimator;
+import android.app.*;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.*;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.*;
+import android.graphics.*;
+import android.graphics.drawable.*;
+import android.media.*;
+import android.net.*;
+import android.net.Uri;
+import android.os.*;
+import android.os.Bundle;
+import android.text.*;
+import android.text.style.*;
+import android.util.*;
+import android.view.*;
+import android.view.View;
+import android.view.View.*;
+import android.view.animation.*;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.BounceInterpolator;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.LinearInterpolator;
+import android.webkit.*;
+import android.widget.*;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import androidx.annotation.*;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import java.io.*;
+import java.io.InputStream;
+import java.text.*;
+import java.util.*;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.regex.*;
+import org.json.*;
+
+public class PinVerificationView extends LinearLayout {
+	
+	private Timer _timer = new Timer();
+	
+	private String str_text1 = "";
+	private String fontName = "";
+	private String typeace = "";
+	private boolean isVisible = false;
+	
+	private LinearLayout main;
+	private LinearLayout toolbar;
+	private LinearLayout keypad;
+	private LinearLayout layer_2;
+	private TextView title_txt;
+	private TextView passcode_txt;
+	private LinearLayout linearLayout;
+	private LinearLayout indicator;
+	private GridLayout keypad_2;
+	private LinearLayout pin1;
+	private LinearLayout pin2;
+	private LinearLayout pin3;
+	private LinearLayout pin4;
+	private TextView num_1;
+	private TextView num_2;
+	private TextView num_3;
+	private TextView num_4;
+	private TextView num_5;
+	private TextView num_6;
+	private TextView num_7;
+	private TextView num_8;
+	private TextView num_9;
+	private LinearLayout reset_lin;
+	private TextView num_0;
+	private LinearLayout backspace_lin;
+	private ImageView reset_img;
+	private ImageView backspace_img;
+	
+	private ObjectAnimator an = new ObjectAnimator();
+	private TimerTask delay;
+    private Context mContext;
+	
+	public PinVerificationView(Context context) {
+		super(context);
+        mContext = context;
+		init(context);
+	}
+	
+	public PinVerificationView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+        mContext = context;
+		init(context);
+	}
+	
+	private void init(Context context) {
+		inflate(context, R.layout.hosted_pin_ui, this);
+		initialize();
+		initializeLogic();
+	}
+	
+	private void initialize() {
+		main = findViewById(R.id.main);
+		toolbar = findViewById(R.id.toolbar);
+		keypad = findViewById(R.id.keypad);
+		layer_2 = findViewById(R.id.layer_2);
+		title_txt = findViewById(R.id.title_txt);
+		passcode_txt = findViewById(R.id.passcode_txt);
+		linearLayout = findViewById(R.id.linearLayout);
+		indicator = findViewById(R.id.indicator);
+		keypad_2 = findViewById(R.id.keypad_2);
+		pin1 = findViewById(R.id.pin1);
+		pin2 = findViewById(R.id.pin2);
+		pin3 = findViewById(R.id.pin3);
+		pin4 = findViewById(R.id.pin4);
+		num_1 = findViewById(R.id.num_1);
+		num_2 = findViewById(R.id.num_2);
+		num_3 = findViewById(R.id.num_3);
+		num_4 = findViewById(R.id.num_4);
+		num_5 = findViewById(R.id.num_5);
+		num_6 = findViewById(R.id.num_6);
+		num_7 = findViewById(R.id.num_7);
+		num_8 = findViewById(R.id.num_8);
+		num_9 = findViewById(R.id.num_9);
+		reset_lin = findViewById(R.id.reset_lin);
+		num_0 = findViewById(R.id.num_0);
+		backspace_lin = findViewById(R.id.backspace_lin);
+		reset_img = findViewById(R.id.reset_img);
+		backspace_img = findViewById(R.id.backspace_img);
+		
+		num_1.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				_Pass(num_1.getText().toString(), num_1.getText().toString());
+			}
+		});
+		
+		num_2.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				_Pass(num_2.getText().toString(), num_2.getText().toString());
+			}
+		});
+		
+		num_3.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				_Pass(num_3.getText().toString(), num_3.getText().toString());
+			}
+		});
+		
+		num_4.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				_Pass(num_4.getText().toString(), num_4.getText().toString());
+			}
+		});
+		
+		num_5.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				_Pass(num_5.getText().toString(), num_5.getText().toString());
+			}
+		});
+		
+		num_6.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				_Pass(num_6.getText().toString(), num_6.getText().toString());
+			}
+		});
+		
+		num_7.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				_Pass(num_7.getText().toString(), num_7.getText().toString());
+			}
+		});
+		
+		num_8.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				_Pass(num_8.getText().toString(), num_8.getText().toString());
+			}
+		});
+		
+		num_9.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				_Pass(num_9.getText().toString(), num_9.getText().toString());
+			}
+		});
+		
+		reset_lin.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				passcode_txt.setText("Enter New Passcode");
+				reset_lin.setVisibility(View.INVISIBLE);
+				str_text1 = "";
+				
+			}
+		});
+		
+		num_0.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				_Pass(num_0.getText().toString(), num_0.getText().toString());
+			}
+		});
+		
+		backspace_lin.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				if (str_text1.length() == 1) {
+					str_text1 = "";
+					pin1.setBackground(new GradientDrawable() { public GradientDrawable getIns(int a, int b, int c, int d) { this.setCornerRadius(a); this.setStroke(b, c); this.setColor(d); return this; } }.getIns((int)360, (int)2, 0xFF3F51B5, Color.TRANSPARENT));
+				}
+				if (str_text1.length() == 2) {
+					str_text1 = str_text1.substring((int)(0), (int)(1));
+					pin2.setBackground(new GradientDrawable() { public GradientDrawable getIns(int a, int b, int c, int d) { this.setCornerRadius(a); this.setStroke(b, c); this.setColor(d); return this; } }.getIns((int)360, (int)2, 0xFF3F51B5, Color.TRANSPARENT));
+				}
+				if (str_text1.length() == 3) {
+					str_text1 = str_text1.substring((int)(0), (int)(2));
+					pin3.setBackground(new GradientDrawable() { public GradientDrawable getIns(int a, int b, int c, int d) { this.setCornerRadius(a); this.setStroke(b, c); this.setColor(d); return this; } }.getIns((int)360, (int)2, 0xFF3F51B5, Color.TRANSPARENT));
+				}
+				if (str_text1.length() == 4) {
+					str_text1 = str_text1.substring((int)(0), (int)(3));
+					pin4.setBackground(new GradientDrawable() { public GradientDrawable getIns(int a, int b, int c, int d) { this.setCornerRadius(a); this.setStroke(b, c); this.setColor(d); return this; } }.getIns((int)360, (int)2, 0xFF3F51B5, Color.TRANSPARENT));
+				}
+			}
+		});
+	}
+	
+	private void initializeLogic() {
+		_UiDesign();
+	}
+	
+	public void _UiDesign() {
+		main.setBackgroundColor(0xFFFFFFFF);
+		_rippleRoundStroke(num_1, "#E0E0E0", "#FFFFFF", 100, 0, "#FFFFFF");
+		_rippleRoundStroke(num_2, "#E0E0E0", "#FFFFFF", 100, 0, "#FFFFFF");
+		_rippleRoundStroke(num_3, "#E0E0E0", "#FFFFFF", 100, 0, "#FFFFFF");
+		_rippleRoundStroke(num_4, "#E0E0E0", "#FFFFFF", 100, 0, "#FFFFFF");
+		_rippleRoundStroke(num_5, "#E0E0E0", "#FFFFFF", 100, 0, "#FFFFFF");
+		_rippleRoundStroke(num_6, "#E0E0E0", "#FFFFFF", 100, 0, "#FFFFFF");
+		_rippleRoundStroke(num_7, "#E0E0E0", "#FFFFFF", 100, 0, "#FFFFFF");
+		_rippleRoundStroke(num_8, "#E0E0E0", "#FFFFFF", 100, 0, "#FFFFFF");
+		_rippleRoundStroke(num_9, "#E0E0E0", "#FFFFFF", 100, 0, "#FFFFFF");
+		_rippleRoundStroke(num_0, "#E0E0E0", "#FFFFFF", 100, 0, "#FFFFFF");
+		_rippleRoundStroke(backspace_lin, "#3F51B5", "#FFFFFF", 100, 0, "#FFFFFF");
+		_rippleRoundStroke(reset_lin, "#3F51B5", "#FFFFFF", 100, 0, "#FFFFFF");
+		indicator.setBackgroundColor(0xFFE0E0E0);
+		num_1.setTextColor(0xFF000000);
+		num_2.setTextColor(0xFF000000);
+		num_3.setTextColor(0xFF000000);
+		num_4.setTextColor(0xFF000000);
+		num_5.setTextColor(0xFF000000);
+		num_6.setTextColor(0xFF000000);
+		num_7.setTextColor(0xFF000000);
+		num_8.setTextColor(0xFF000000);
+		num_9.setTextColor(0xFF000000);
+		num_0.setTextColor(0xFF000000);
+		pin1.setBackground(createStrokeCircleDrawable(0xFF3F51B5, 2));
+		pin2.setBackground(createStrokeCircleDrawable(0xFF3F51B5, 2));
+		pin3.setBackground(createStrokeCircleDrawable(0xFF3F51B5, 2));
+		pin4.setBackground(createStrokeCircleDrawable(0xFF3F51B5, 2));
+	}
+	
+	
+	public void _rippleRoundStroke(final View _view, final String _focus, final String _pressed, final double _round, final double _stroke, final String _strokeclr) {
+		android.graphics.drawable.GradientDrawable GG = new android.graphics.drawable.GradientDrawable();
+		GG.setColor(Color.parseColor(_focus));
+		GG.setCornerRadius((float)_round);
+		GG.setStroke((int) _stroke,
+		Color.parseColor("#" + _strokeclr.replace("#", "")));
+		android.graphics.drawable.RippleDrawable RE = new android.graphics.drawable.RippleDrawable(new android.content.res.ColorStateList(new int[][]{new int[]{}}, new int[]{ Color.parseColor(_pressed)}), GG, null);
+		_view.setBackground(RE);
+	}
+	
+	
+	public void _Pass(final String _txt, final String _txt2) {
+		if (str_text1.length() == 0) {
+			str_text1 = str_text1.concat(_txt);
+			pin1.setBackground(new GradientDrawable() { public GradientDrawable getIns(int a, int b) { this.setCornerRadius(a); this.setColor(b); return this; } }.getIns((int)360, 0xFF3F51B5));
+		} else {
+			if (str_text1.length() == 1) {
+				str_text1 = str_text1.concat(_txt);
+				pin2.setBackground(new GradientDrawable() { public GradientDrawable getIns(int a, int b) { this.setCornerRadius(a); this.setColor(b); return this; } }.getIns((int)360, 0xFF3F51B5));
+			} else {
+				if (str_text1.length() == 2) {
+					str_text1 = str_text1.concat(_txt);
+					pin3.setBackground(new GradientDrawable() { public GradientDrawable getIns(int a, int b) { this.setCornerRadius(a); this.setColor(b); return this; } }.getIns((int)360, 0xFF3F51B5));
+				} else {
+					if (str_text1.length() == 3) {
+						str_text1 = str_text1.concat(_txt);
+						pin4.setBackground(createCircleDrawable(0xFF3F51B5));
+						delay = new TimerTask() {
+							@Override
+							public void run() {
+								((Activity) mContext).runOnUiThread(new Runnable() {
+									@Override
+									public void run() {
+										pin1.setBackground(createStrokeCircleDrawable(0xFF3F51B5, 2));
+										pin2.setBackground(createStrokeCircleDrawable(0xFF3F51B5, 2));
+										pin3.setBackground(createStrokeCircleDrawable(0xFF3F51B5, 2));
+										pin4.setBackground(createStrokeCircleDrawable(0xFF3F51B5, 2));
+										if (pinCompleteListener != null) {
+											pinCompleteListener.onPinComplete(str_text1); 
+										}
+										str_text1 = "";
+									}
+								});
+							}
+						};
+						_timer.schedule(delay, (int)(300));
+					}
+				}
+			}
+		}
+	}
+	
+	
+	public void _extra() {
+	}
+	
+	public void shake() {
+		ObjectAnimator animator = ObjectAnimator.ofFloat(this, "translationX", 0, 10, -10, 10, -10, 0);
+		animator.setDuration(500);
+		animator.setInterpolator(new CycleInterpolator(5));
+		animator.start();
+	}
+	
+	// 1. Create Interface
+	public interface OnPinCompleteListener {
+		void onPinComplete(String pin);
+	}
+	
+	// 2. Create Variable
+	private OnPinCompleteListener pinCompleteListener;
+	
+	// 3. Create Setter
+	public void setOnPinCompleteListener(OnPinCompleteListener listener) {
+		this.pinCompleteListener = listener;
+		
+	}
+	
+	private GradientDrawable createCircleDrawable(int color) {
+		GradientDrawable drawable = new GradientDrawable();
+		drawable.setCornerRadius(360);
+		drawable.setColor(color);
+		return drawable;
+	}
+	
+	private GradientDrawable createStrokeCircleDrawable(int strokeColor, int strokeWidth) {
+		GradientDrawable drawable = new GradientDrawable();
+		drawable.setCornerRadius(360);
+		drawable.setStroke(strokeWidth, strokeColor);
+		drawable.setColor(Color.TRANSPARENT);
+		return drawable;
+		
+	}
+	
+}
--- README.md
+++ README.md
@@ -1,1 +1,133 @@
-# PinViewLib
+# PinVerificationView
+
+A clean, customizable PIN entry view for Android apps with PIN validation, animations, and easy event handling.
+
+This library helps you quickly integrate a beautiful 4-digit PIN screen with auto UI feedback and a shake animation for wrong attempts.
+
+[![](https://jitpack.io/v/oyin25/PinVerificationLib.svg)](https://jitpack.io/#oyin25/PinVerificationLib)
+
+---
+
+## Features
+
+- 4-digit PIN entry UI
+- Auto-detect PIN completion
+- Easy PIN validation with `setOnPinCompleteListener`
+- Shake animation for wrong PIN
+- Customizable layout and styles
+- Lightweight and easy to integrate
+
+---
+
+## Installation
+
+### Step 1: Add JitPack to your root `settings.gradle`
+
+Groovy (settings.gradle):
+```groovy
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url 'https://jitpack.io' }
+    }
+}
+```
+
+Kotlin DSL (settings.gradle.kts):
+```kotlin
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+    }
+}
+```
+
+---
+
+### Step 2: Add the dependency in your `app/build.gradle`
+
+```groovy
+dependencies {
+    implementation 'com.github.oyin25:PinVerificationLib:v1.0.0'
+}
+```
+
+---
+
+## Usage
+
+### Step 1: Add the view to your XML layout
+
+```xml
+<com.HP.Verification.PinVerificationView
+    android:id="@+id/pin_view"
+    android:layout_width="match_parent"
+    android:layout_height="wrap_content" />
+```
+
+---
+
+### Step 2: Access it in your Activity
+
+```java
+PinVerificationView pinView = findViewById(R.id.pin_view);
+
+pinView.setOnPinCompleteListener(new PinVerificationView.OnPinCompleteListener() {
+    @Override
+    public void onPinComplete(String pin) {
+        Toast.makeText(MainActivity.this, "Pin entered: " + pin, Toast.LENGTH_SHORT).show();
+
+        // Example: Check if pin is correct
+        if (pin.equals("1234")) {
+            Toast.makeText(MainActivity.this, "Correct PIN!", Toast.LENGTH_SHORT).show();
+        } else {
+            pinView.shake(); // Shake animation on wrong PIN
+        }
+    }
+});
+```
+
+---
+
+## Important
+
+- The library **auto detects** when 4 digits are entered.
+- Call `shake()` manually if you want to show wrong PIN effect.
+- You can customize the design by modifying the hosted_pin_ui.xml file.
+
+---
+
+## Screenshots
+
+(Add your screenshots or GIF demos here if available)
+
+---
+
+## License
+
+MIT License
+
+```
+MIT License
+
+Copyright (c) 2025 oyin25
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions...
+```
+
+---
+
+## Author
+
+Made with passion by [@oyin25](https://github.com/oyin25)
+
